6244d5cb051ff92b876566db0a960a8995346b45
Isaiah Chan <zcabyrc@ucl.ac.uk>
2026-06-15 16:03:42 +0800

    Added SDL initialisation and quitting

--- chip8.c
+++ chip8.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
+
+bool init_sdl(void){
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0){
+        SDL_Log("Could not initialize SDL subsystems %s\n", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void final_cleanup(void){
+    SDL_Quit();
+}
 
 int main(int argc, char **argv){
     (void)argc;
     (void)argv;
+
+    //Initialize SDL
+    //
+
+    //Final cleanup
+    //
 
     puts("testing on linux");
 }
